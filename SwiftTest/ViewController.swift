52da1f14acc3bd067535cba0456285c808871b56
yomura-cq <y.omura@caraquri.com>
2019-05-26 14:48:33 +0900

    Initial Commit

---
+++ SwiftTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by オムラユウキ on 2019/05/26.
+//  Copyright © 2019 Swifter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
